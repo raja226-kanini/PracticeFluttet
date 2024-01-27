c73320e5bb2698b1e8421f14f9eb4c55b11eff0c
raja226-kanini <144207791+raja226-kanini@users.noreply.github.com>
2024-01-27 18:04:04 +0530

    login page UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:paractice_flutter/views/getstart_screen.dart';
+import 'package:paractice_flutter/views/login_screen.dart';
 import 'package:paractice_flutter/views/registration_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-     home: GetStartScreen(),
+     home: LoginScreen(),
      debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/views/login_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Enter your mobile number",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.poppins(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: -0.8,
+                ),
+              ),
+              Text("We will send you an otp for verification",
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.4,
+                    color: const Color(0xFF9EA8B3),
+                  )),
+              const SizedBox(
+                height: 21,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: const Color(0xFFE9F3F9),
+                ),
+                child: TextFormField(
+                    keyboardType: TextInputType.phone,
+                    maxLength: 10,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        counterText: "",
+                        border: InputBorder.none,
+                        contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 1.0),
+                        hintText: 'Phone number',
+                        hintStyle: GoogleFonts.manrope(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: -0.8,
+                        ),
+                        prefix: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "+91",
+                            style: GoogleFonts.manrope(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: -0.8,
+                            ),
+                          ),
+                        ))),
+              ),
+              const SizedBox(
+                height: 150,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Get OTP",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -0.17,
+                      color: Colors.white,
+                      backgroundColor: const Color(0xFF931880),
+                    ),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0xFF931880)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don’t have an account? ",
+                      style: TextStyle(
+                        color: const Color(0xFF7E8B99),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("Register option Tapped");
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
